31aed6e6f66ac39afe53ae61ae6b69fb78626fb9
pawan <pawan.bansari@agileinfoways.com>
2025-02-11 11:27:41 +0530

    removing db connections

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user.entity';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { User } from './user.entity';
 // import { DatabaseService } from '../src/database.service'; // Ensure this file exists
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'test-db.ctcmqkco6h9r.ap-south-1.rds.amazonaws.com', // e.g., database-1.abcdefg.us-east-1.rds.amazonaws.com
-      port: 3306,
-      username: 'admin',
-      password: 'Agile#1357',
-      database: 'your_database',
-      entities: [User], // Add your entities here
-      synchronize: true, // Set to false in production
-    }),
-    TypeOrmModule.forFeature([User]), // Register the entity if needed
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'test-db.ctcmqkco6h9r.ap-south-1.rds.amazonaws.com', // e.g., database-1.abcdefg.us-east-1.rds.amazonaws.com
+    //   port: 3306,
+    //   username: 'admin',
+    //   password: 'Agile#1357',
+    //   database: 'your_database',
+    //   entities: [User], // Add your entities here
+    //   synchronize: true, // Set to false in production
+    // }),
+    // TypeOrmModule.forFeature([User]), // Register the entity if needed
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,21 +1,23 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+// import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 
 @Injectable()
-export class AppService implements OnModuleInit {
+// export class AppService implements OnModuleInit {
+export class AppService {
   constructor(private dataSource: DataSource) {}
 
   getHello(): string {
     return 'Hello World!';
   }
 
-  async onModuleInit() {
-    try {
-      if (this.dataSource.isInitialized) {
-        console.log('✅ Successfully connected to the RDS MySQL database!');
-      }
-    } catch (error) {
-      console.error('❌ Failed to connect to the RDS MySQL database:', error);
-    }
-  }
+  // async onModuleInit() {
+  //   try {
+  //     if (this.dataSource.isInitialized) {
+  //       console.log('✅ Successfully connected to the RDS MySQL database!');
+  //     }
+  //   } catch (error) {
+  //     console.error('❌ Failed to connect to the RDS MySQL database:', error);
+  //   }
+  // }
 }
